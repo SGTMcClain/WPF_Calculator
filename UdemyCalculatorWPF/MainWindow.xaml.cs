aabe256a9b9e030e53416caaaaa0fcfdc546b08e
Nathan McClain <mcclain_nathan@bah.com>
2019-06-26 12:30:05 -0400

    Add project files.

---
+++ UdemyCalculatorWPF/MainWindow.xaml.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UdemyCalculatorWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double lastNumber, result;
+        SelectedOperator selectedOperator;
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            acButton.Click += AcButton_Click;
+            negativeButton.Click += NegativeButton_Click;
+            percentButton.Click += PercentButton_Click;
+            equalsButton.Click += EqualsButton_Click;
+        }
+
+        private void EqualsButton_Click(object sender, RoutedEventArgs e)
+        {
+            double newNumber;
+            if (double.TryParse(resultLabel.Content.ToString(), out newNumber))
+            {
+                switch (selectedOperator)
+                {
+                    case SelectedOperator.Addition:
+                        break;
+                    case SelectedOperator.Subtraction:
+                        break;
+                    case SelectedOperator.Multiplication:
+                        break;
+                    case SelectedOperator.Division:
+                        break;
+                }
+            }
+
+        }
+
+        private void PercentButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber / 100;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void NegativeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber * -1;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void AcButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine(sender.ToString());
+            resultLabel.Content = "0";
+        }
+
+        // number buttons
+
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                resultLabel.Content = "0";
+            }
+
+            if (sender.Equals(multiplyButton)) selectedOperator = SelectedOperator.Multiplication;
+            if (sender.Equals(addButton)) selectedOperator = SelectedOperator.Addition;
+            if (sender.Equals(subtractButton)) selectedOperator = SelectedOperator.Subtraction;
+            if (sender.Equals(divideButton)) selectedOperator = SelectedOperator.Division;
+        }
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            int selectedValue = 0;
+
+            if (sender.Equals(zeroButton))  selectedValue = 0;
+            if (sender.Equals(oneButton))   selectedValue = 1;
+            if (sender.Equals(twoButton))   selectedValue = 2;
+            if (sender.Equals(threeButton)) selectedValue = 3;
+            if (sender.Equals(fourButton))  selectedValue = 4;
+            if (sender.Equals(fiveButton))  selectedValue = 5;
+            if (sender.Equals(sixButton))   selectedValue = 6;
+            if (sender.Equals(sevenButton)) selectedValue = 7;
+            if (sender.Equals(eightButton)) selectedValue = 8;
+            if (sender.Equals(nineButton))  selectedValue = 9;
+
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = $"{selectedValue}";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}{selectedValue}";
+            }
+        }
+    }
+
+    public enum SelectedOperator
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division
+    }
+
+    public class SimpleMath
+    {
+        public double Add(double firstNumber, double secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public double Subtract(double firstNumber, double secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+
+        public double Multiply(double firstNumber, double secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+
+        public double Divide(double firstNumber, double secondNumber)
+        {
+            return firstNumber / secondNumber;
+        }
+    }
+}
